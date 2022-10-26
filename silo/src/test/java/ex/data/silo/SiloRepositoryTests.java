c68a018f61981976d2c1b5146b163cd186ecebd5
Mario Gray <mario.gray@gmail.com>
2022-10-26 00:39:35 -0700

    matcher silo

--- silo/src/test/java/ex/data/silo/SiloRepositoryTests.java
+++ silo/src/test/java/ex/data/silo/SiloRepositoryTests.java
@@ -8,6 +8,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
@@ -23,32 +25,60 @@ public class SiloRepositoryTests {
     void test_should_save_find_messages() {
         var message = new Message(null, 1L, 2L, "TEST");
 
-        var stream = messages.save(message)
-                .then(messages.findById(1L));
+        var flux = messages
+                .save(message)
+                .thenMany(messages.findAll());
 
         StepVerifier
-                .create(stream)
-                .assertNext(msg -> {
+                .create(flux)
+                .consumeNextWith(msg -> {
                     Assertions
                             .assertThat(msg)
                             .isNotNull()
-                            .hasNoNullFieldsOrProperties()
-                            .hasFieldOrPropertyWithValue("id", 1L);   // HINT
+                            .hasNoNullFieldsOrProperties();
                 })
                 .verifyComplete();
     }
+
     @Test
     void test_should_save_find_users() {
         var user = new User(null, "L.Skywalker");
 
+        var flux = users
+                .save(user)
+                .thenMany(users.findAll());
+
         StepVerifier
-                .create(users.save(user))
-                .assertNext(u -> {
+                .create(flux)
+                .consumeNextWith(u -> {
                     Assertions
                             .assertThat(u)
                             .isNotNull()
                             .hasNoNullFieldsOrProperties();
                 })
+                .verifyComplete();
+    }
+
+    @Test
+    void test_should_find_user_by_example() {
+        var user = new User(null, "D.Vader");
+
+        var example = Example.of(user, ExampleMatcher
+                .matching().withIgnorePaths("id"));
+
+        var flux = users
+                .save(user)
+                .thenMany(users.findOne(example));
+
+        StepVerifier
+                .create(flux)
+                .consumeNextWith(u ->{
+                    Assertions
+                            .assertThat(u)
+                            .isNotNull()
+                            .hasNoNullFieldsOrProperties()
+                            .hasFieldOrPropertyWithValue("name", "D.Vader");
+                        })
                 .verifyComplete();
     }
 }
